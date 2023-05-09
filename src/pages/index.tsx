37a5c65617bc68da833159744f91d594135593f2
smartman0307 <skybolt0307@gmail.com>
2023-05-09 11:44:15 +0200

    feat: :sparkles: implement delete post

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -18,7 +18,6 @@ import {
   Image,
   ScrollArea,
   Text,
-  TextInput,
   Title,
 } from "@mantine/core";
 import { IMAGE_MIME_TYPE } from "@mantine/dropzone";
@@ -72,25 +71,11 @@ export default function Home({ signUpEnabled }: HomeProps) {
   return (
     <>
       <Head />
-
       <Layout
         signUpEnabled={signUpEnabled}
         onFiles={(files) => user && createPost(files)}
       >
         <Container>
-          <Box p="lg" mb="md">
-            <form action="search" method="get">
-              <TextInput
-                id="query"
-                name="query"
-                placeholder="Search"
-                size="lg"
-                radius="xl"
-                variant="filled"
-              />
-            </form>
-          </Box>
-
           <Flex sx={{ justifyContent: "space-between" }}>
             <Title order={2}>Latest Posts</Title>
             <Anchor component={Link} href="/posts">
